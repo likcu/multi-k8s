705a32b4175f39605ad02ef77d0fdf016d615940
Jiatong Li <li.jiato@husky.neu.edu>
2020-01-01 00:54:04 -0800

    update deploye.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t likcu/multi-client:latest -t likcu/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t likcu/multi-server:latest -t likcu/multi-server:$SHA -f ./client/Dockerfile ./client
-docker build -t likcu/multi-worker:latest -t likcu/multi-worker:$SHA -f ./client/Dockerfile ./client
-docker push likcu/multi-client:latest
-docker push likcu/multi-server:latest
-docker push likcu/multi-worker:latest
-docker push likcu/multi-client:$SHA
-docker push likcu/multi-server:$SHA
-docker push likcu/multi-worker:$SHA
+docker build -t jessielee716/multi-client:latest -t jessielee716/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jessielee716/multi-server:latest -t jessielee716/multi-server:$SHA -f ./client/Dockerfile ./client
+docker build -t jessielee716/multi-worker:latest -t jessielee716/multi-worker:$SHA -f ./client/Dockerfile ./client
+docker push jessielee716/multi-client:latest
+docker push jessielee716/multi-server:latest
+docker push jessielee716/multi-worker:latest
+docker push jessielee716/multi-client:$SHA
+docker push jessielee716/multi-server:$SHA
+docker push jessielee716/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=likcu/multi-server:$SHA
-kubectl set image deployments/client-deployment client=likcu/multi-client:$SHA
-kubectl set image deployments/client-deployment client=likcu/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=jessielee716/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jessielee716/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jessielee716/multi-worker:$SHA
